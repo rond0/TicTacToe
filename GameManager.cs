41e55e00c0bad0359eed2b00d33bf5f0f9a75b2b
rindy <46899557+rond0@users.noreply.github.com>
2022-03-27 01:01:31 +0300

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Protocol
+    /*
+     * This is a proposal only
+     Message = playerID [space] positionID
+
+     int that represents position of the player's step.
+     [0][1][2]
+     [3][4][5]
+     [6][7][8]
+     */
+    #endregion
+    char[] Gameboard = new char[] { '_', '_', '_', '_', '_', '_', '_', '_', '_' };
+    string Boardstate;
+    private void Start()
+    {
+
+        DrawBoard();
+        //networkManager.StartUDP();
+    }
+
+    public NetworkManager networkManager; //don't forget to drag in inspector
+    public char Player;
+    public void DrawBoard() {
+        Boardstate = string.Format("{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}", Gameboard[0], Gameboard[1], Gameboard[2], Gameboard[3], Gameboard[4], Gameboard[5], Gameboard[6], Gameboard[7], Gameboard[8]);
+        Debug.Log(Boardstate);
+    }
+    public void GotNetworkMessage(string message)
+    {
+        Debug.Log("got network message: " + message);
+       if (Player == 'X')
+        {
+            Gameboard[int.Parse(message)] = 'O';
+        }
+       else if (Player == 'O')
+        {
+            Gameboard[int.Parse(message)] = 'X';
+        }
+        DrawBoard();
+    }
+
+    public void PositionClicked(int position)
+    {
+        //draw the shape on the UI
+
+        //update the other player about the shape
+        networkManager.SendMessage("");// your job to finish it
+    }
+
+    //for debug purpouses only
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            // networkManager.SendMessage("A was sent");
+            Debug.Log("X selected");
+            Player = 'X';
+            networkManager.StartUDP();
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            // networkManager.SendMessage("B was sent");
+            Debug.Log("O selected");
+            Player = 'O';
+            networkManager.ListeningPort = 40001;
+            networkManager.SendingPort = 40000;
+            networkManager.StartUDP();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad7))
+        {
+            Gameboard[0] = Player;
+            networkManager.SendMessage("0");
+            DrawBoard();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            Gameboard[1] = Player;
+            networkManager.SendMessage("1");
+            DrawBoard();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            Gameboard[2] = Player;
+            networkManager.SendMessage("2");
+            DrawBoard();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            Gameboard[3] = Player;
+            networkManager.SendMessage("3");
+            DrawBoard();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            Gameboard[4] = Player;
+            networkManager.SendMessage("4");
+            DrawBoard();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            Gameboard[5] = Player;
+            networkManager.SendMessage("5");
+            DrawBoard();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Gameboard[6] = Player;
+            networkManager.SendMessage("6");
+            DrawBoard();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            Gameboard[7] = Player;
+            networkManager.SendMessage("7");
+            DrawBoard();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            Gameboard[8] = Player;
+            networkManager.SendMessage("8");
+            DrawBoard();
+        }
+
+    }
+}
---
+++ NetworkManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Text;
+using System.Threading;
+
+public class NetworkManager : MonoBehaviour
+{
+    private string LocalIPAddress = "127.0.0.1";
+    public int ListeningPort = 40000;
+    public int SendingPort = 40001;
+    Thread listener;
+    static Queue pQueue = Queue.Synchronized(new Queue()); //this is the message queue, it is thread safe
+    static UdpClient udp;
+    private IPEndPoint endPoint;
+
+    public GameManager gameManager; //drag this on the inspector
+
+    private void Update()
+    {
+        //in the main thread, read the message and update the game manager
+        lock (pQueue.SyncRoot)
+        {
+            if (pQueue.Count > 0)
+            {
+                object o = pQueue.Dequeue(); //Take the olders message out of the queue
+                gameManager.GotNetworkMessage((string)o); //Send it to the game manager
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        EndUDP();
+    }
+
+    public void StartUDP()
+    {
+        endPoint = new IPEndPoint(IPAddress.Any, ListeningPort); //this line will listen to all IP addresses in the network
+        //endPoint = new IPEndPoint(IPAddress.Parse(LocalIPAddress), ListeningPort); //this line will listen to a specific IP address
+        udp = new UdpClient(endPoint);
+        Debug.Log("Listening for Data...");
+        listener = new Thread(new ThreadStart(MessageHandler));
+        listener.IsBackground = true;
+        listener.Start();
+    }
+
+    void MessageHandler()
+    {
+        Byte[] data = new byte[0];
+        while (true)
+        {
+            try
+            {
+                //Did we get a new message?
+                data = udp.Receive(ref endPoint);
+            }
+            catch (Exception err)
+            {
+                //If there's a problem
+                Debug.Log("Communication error, recieve data error " + err);
+                udp.Close();
+                return;
+            }
+            //Treat the new message
+            string msg = Encoding.ASCII.GetString(data);
+            Debug.Log("UDP incoming " + msg);
+            pQueue.Enqueue(msg);
+        }
+    }
+
+    private void EndUDP()
+    {
+        if (udp != null)
+        {
+            udp.Close();
+        }
+        if (listener != null)
+        {
+            listener.Abort();
+        }
+    }
+
+    public void SendMessage(string message)
+    {
+        UdpClient send_client = new UdpClient();
+        IPEndPoint send_endPoint = new IPEndPoint(IPAddress.Parse(LocalIPAddress), SendingPort);
+        byte[] bytes = Encoding.ASCII.GetBytes(message);
+        send_client.Send(bytes, bytes.Length, send_endPoint);
+        send_client.Close();
+        Debug.Log("Sent message: " + message);
+    }
+
+
+}
